d527e3cf88cd5f7b64ff8c52bc47c9d05f497f54
Jesus Flores <4wtx4mbx85@privaterelay.appleid.com>
2026-05-29 11:58:38 -0400

    Fix GitHub Pages base path

--- astro.config.ts
+++ astro.config.ts
@@ -4,10 +4,12 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://nexus-group-lap.github.io',
+  base: '/flores-nexus.github.io',
   integrations: [icon()],
   server: {
     host: '0.0.0.0',
     port: 5000,
-    allowedHosts: true, // don't touch this!
+    allowedHosts: true, // don’t touch this!
   },
 });
