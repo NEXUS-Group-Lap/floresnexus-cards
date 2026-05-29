3ef783064aed2c4f80580b068687eceb75233b54
Jesus Flores <4wtx4mbx85@privaterelay.appleid.com>
2026-05-29 17:20:56 -0400

    Fix custom domain asset paths

--- astro.config.ts
+++ astro.config.ts
@@ -4,12 +4,11 @@ import icon from 'astro-icon';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://nexus-group-lap.github.io',
-  base: '/flores-nexus.github.io',
+  site: 'https://floresnexus.com',
   integrations: [icon()],
   server: {
     host: '0.0.0.0',
     port: 5000,
-    allowedHosts: true, // don’t touch this!
+    allowedHosts: true,
   },
 });
